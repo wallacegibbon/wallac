1e9e9d01eaa1216e9253f9c96c3916fdcee16041
wallace <wallacegibbon@aliyun.com>
2018-01-13 12:37:42 +0800

    removed some unused old variables

--- src/vars.h
+++ src/vars.h
@@ -1,15 +1,9 @@
 #ifndef __VARS_H__
 #define __VARS_H__
 
-#include <stdio.h>
 
 extern char *filename_src, *filename_out;
-
 extern int verbose;
-
-extern FILE *fp_in, *fp_out;
-
-extern int current_line;
 
 
 #endif
